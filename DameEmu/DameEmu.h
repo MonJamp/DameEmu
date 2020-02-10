51af61e0d596ab3445b4470941410291be755c2a
Filjo Abraham <filjoabraham@gmail.com>
2020-02-09 23:11:54 -0600

    Correct memory map

--- DameEmu/DameEmu.h
+++ DameEmu/DameEmu.h
@@ -28,10 +28,10 @@ enum EmuStatus {
 
 class DameEmu {
 public:
-	DameEmu(const char* ROM_DIR, const char* BIOS_DIR);
+	DameEmu(const char* ROM_DIR);
 	~DameEmu();
 
-	void BootUp(const char* ROM_DIR, const char* BIOS_DIR);
+	void BootUp(const char* ROM_DIR);
 	EmuStatus Cycle();
 
 	sf::RenderWindow* GetApp() { return app; };
@@ -91,19 +91,19 @@ private:
 
 	//Memory map
 	union {
-		uint8_t memory[0xFFFF];
+		uint8_t memory[0x10000];
 		struct {
 			uint8_t ROM[0x8000];			//0000h - 7FFFh
-			uint8_t vRAM[0x2000];			//8000h - 9FFFh
+			uint8_t VRAM[0x2000];			//8000h - 9FFFh
 			uint8_t externalRAM[0x2000];	//A000h - BFFFh
-			uint8_t workRAM[0x2000];		//C000h - DFFFh
-			uint8_t echoRAM[0x1DFF];		//E000h - FDFFh
-			//Internal CPU RAM
-			uint8_t oam[0xA0];				//FE00h - FE9Fh
+			uint8_t WRAM[0x2000];			//C000h - DFFFh
+			uint8_t echoRAM[0x1E00];		//E000h - FDFFh
+			uint8_t OAM[0xA0];				//FE00h - FE9Fh
 			uint8_t unusable[0x60];			//FEA0h - FEFFh
-			uint8_t ioRegisters[0x80];		//FF00h - FF7Fh
-			uint8_t stackRAM[0x7F];			//FF80h - FFFEh
-			uint8_t interruptsEnable;		//FFFFh - FFFFh
+			uint8_t ioPorts[0x80];			//FF00h - FF7Fh
+			uint8_t highRAM[0x7F];			//FF80h - FFFEh
+			//Interrupt Master Enable
+			uint8_t IME;					//FFFFh - FFFFh
 		};
 	};
 
