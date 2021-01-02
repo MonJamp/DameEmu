c3613a63db7f53bfb3e8c07abbe53cc5e85e0b7d
MonJamp <filjoabraham@gmail.com>
2021-01-02 14:24:20 -0600

    Implement open file dialog

--- GUI/AppMain.h
+++ GUI/AppMain.h
@@ -1,5 +1,14 @@
 #pragma once
+
 #include "../DameEmu.h"
+
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include "Add-ons/imfilebrowser.h"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include <array>
 #include <memory>
 
 
@@ -16,4 +25,6 @@ public:
 private:
 	sf::RenderWindow app;
 	std::unique_ptr<DameEmu> dameEmu;
+
+	ImGui::FileBrowser fileDialog;
 };
